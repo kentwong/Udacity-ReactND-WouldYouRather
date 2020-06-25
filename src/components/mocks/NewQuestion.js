ef0927739c4b50c227ce4f0fcd3c06c6dd65c93e
kentwong <kent_wong@outlook.com>
2020-06-25 19:56:20 +1000

    feat: add NewQuestion page

---
+++ src/components/mocks/NewQuestion.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Segment } from "semantic-ui-react";
+
+export class NewQuestion extends Component {
+  render() {
+    return (
+      <div>
+        <Segment>
+          <h3>New Question</h3>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+export default NewQuestion;
