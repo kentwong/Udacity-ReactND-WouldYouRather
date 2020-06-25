39b6fa6c5103705d441d46136f75bc2eb58ad1c4
kentwong <kent_wong@outlook.com>
2020-06-25 20:11:53 +1000

    feat: add Leader Board page

---
+++ src/components/mocks/LeaderBoard.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import { Segment } from "semantic-ui-react";
+
+export class LeaderBoard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Segment>
+          <h3>Leader Board</h3>
+        </Segment>
+      </Fragment>
+    );
+  }
+}
+
+export default LeaderBoard;
