de970ed1fc569870aa52e597005bb893af569d50
kentwong <kent_wong@outlook.com>
2020-06-29 18:11:20 +1000

    feat: update LeaderBoard mock page

--- src/components/mocks/_data.js
+++ src/components/mocks/_data.js
@@ -54,3 +54,65 @@ export const questionDetails = {
     text: "Become a supervillan.",
   },
 };
+
+export const leaderboardData = [
+  {
+    id: "kentwong",
+    name: "Kent Wong",
+    avatar: "gent1.png",
+    answerCount: 15,
+    questionCount: 6,
+  },
+  {
+    id: "rangerliao",
+    name: "Ranger Liao",
+    avatar: "gent2.png",
+    answerCount: 10,
+    questionCount: 4,
+  },
+  {
+    id: "carrieguo",
+    name: "Carrie Guo",
+    avatar: "lady1.png",
+    answerCount: 7,
+    questionCount: 4,
+  },
+];
+
+export const users = {
+  sarahedo: {
+    id: "sarahedo",
+    name: "Sarah Edo",
+    // avatarURL: ,
+    answers: {
+      "8xf0y6ziyjabvozdd253nd": "optionOne",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+      am8ehyc8byjqgar0jgpub9: "optionTwo",
+      loxhs1bqm25b708cmbf3g: "optionTwo",
+    },
+    questions: ["8xf0y6ziyjabvozdd253nd", "am8ehyc8byjqgar0jgpub9"],
+  },
+  tylermcginnis: {
+    id: "tylermcginnis",
+    name: "Tyler McGinnis",
+    // avatarURL: ,
+    answers: {
+      vthrdm985a262al8qx3do: "optionOne",
+      xj352vofupe1dqz9emx13r: "optionTwo",
+    },
+    questions: ["loxhs1bqm25b708cmbf3g", "vthrdm985a262al8qx3do"],
+  },
+  johndoe: {
+    id: "johndoe",
+    name: "John Doe",
+    // avatarURL: ,
+    answers: {
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+    },
+    questions: ["6ni6ok3ym7mf1p33lnez", "xj352vofupe1dqz9emx13r"],
+  },
+};
+
+// export default userQuestionData;
