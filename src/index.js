58125e7c9daa6faf3ee4439a9b87446a7e1a69e4
kentwong <kent_wong@outlook.com>
2020-07-01 19:16:18 +1000

    feat: add redux provider code to index.js entry point

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
