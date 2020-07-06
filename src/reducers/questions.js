b8337cd6d375c211f46ba4eb7cdc27d8ecca2eec
kentwong <kent_wong@outlook.com>
2020-07-06 20:42:45 +1000

    feat: add new questions to users

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,8 @@
-import { GET_QUESTIONS, ADD_ANSWER_TO_QUESTION } from "../actions/questions";
+import {
+  GET_QUESTIONS,
+  ADD_ANSWER_TO_QUESTION,
+  ADD_QUESTION,
+} from "../actions/questions";
 
 export default function questions(state = {}, action) {
   switch (action.type) {
@@ -19,6 +23,13 @@ export default function questions(state = {}, action) {
             votes: state[qid][answer].votes.concat(loggedUser),
           },
         },
+      };
+    case ADD_QUESTION:
+      const { question } = action;
+
+      return {
+        ...state,
+        [question.id]: question,
       };
     default:
       return state;
