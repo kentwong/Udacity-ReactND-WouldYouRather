fba14027fe06bf0397cb1176289d4bad5bf298f7
kentwong <kent_wong@outlook.com>
2020-07-08 23:05:40 +1000

    feat: format error 404 page

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { Container, Header } from "semantic-ui-react";
 
 class Error404 extends Component {
   render() {
     return (
-      <div>
-        <h3>Error 404 - Page Not Found! </h3>
-      </div>
+      <Container textAlign="center" padded>
+        <Header as="h3">Error 404 - Page Not Found! </Header>
+        <p>Please use menu to navigate to the correct page! </p>
+      </Container>
     );
   }
 }
